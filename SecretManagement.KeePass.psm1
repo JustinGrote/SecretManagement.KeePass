82b0f2d4ced1d6d85c78dcbf1cd9d99420df6e2e
Justin Grote <justingrote@users.noreply.github.com>
2021-01-11 17:59:19 -0800

    Chore: Cleanup unused compatibility function

--- SecretManagement.KeePass.psm1
+++ SecretManagement.KeePass.psm1
@@ -1,14 +1,1 @@
 #This file normally is intentionally left blank for https://github.com/pester/Pester/issues/1456
-function Get-KPSecretManagementConnection {
-    <#
-    .SYNOPSIS
-    Compatibility function for the nested poshkeepass function to fetch the keepass config object
-    #>
-    param (
-        $VaultName
-    )
-    Get-Module (SecretManagement.Keepass.Extension) {
-        (Get-Variable -Scope Script -Name "VAULT_$VaultName").Value
-    }
-    
-}
