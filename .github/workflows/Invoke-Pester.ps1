e26f122fb6fbec314aec8b55d9c3696a0b065f54
Tom Robijns <trammeke@hotmail.com>
2021-01-13 23:36:25 +0100

    stupid thick fingers....

--- .github/workflows/Invoke-Pester.ps1
+++ .github/workflows/Invoke-Pester.ps1
@@ -1,6 +1,5 @@
 Install-Module Pester,Microsoft.Powershell.SecretManagement -Force -Scope CurrentUser
-Write-Verbose "current location is $($PSScriptRoot)" -Verbose
-Set-Location -Path "$($PSScriptRoot)/../../SecredManagement.Keepass.Extension/Tests"
+Set-Location -Path "$($PSScriptRoot)/../../SecretManagement.Keepass.Extension/Tests"
 $pesterResult = Invoke-Pester -Output Detailed -PassThru
 if ($pesterResult.Result -ne 'Passed') {
     throw "There were $($pesterResult.FailedCount) failed tests."
