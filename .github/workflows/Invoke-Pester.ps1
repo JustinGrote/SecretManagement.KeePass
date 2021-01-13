f7cb62a0bdde5d3839ada2d40f7435458c3af48e
Tom Robijns <trammeke@hotmail.com>
2021-01-13 23:30:44 +0100

    force pester to extension folder

--- .github/workflows/Invoke-Pester.ps1
+++ .github/workflows/Invoke-Pester.ps1
@@ -1,4 +1,5 @@
 Install-Module Pester,Microsoft.Powershell.SecretManagement -Force -Scope CurrentUser
+Set-Location -Path "$($PSScriptRoot)/../../SecredManagement.Keepass.Extension/Tests"
 $pesterResult = Invoke-Pester -Output Detailed -PassThru
 if ($pesterResult.Result -ne 'Passed') {
     throw "There were $($pesterResult.FailedCount) failed tests."
