4d14164b5d739e7ca10d218e9b4aa9ef6269e489
Tom Robijns <trammeke@hotmail.com>
2021-01-13 23:33:34 +0100

    Echo psscriptroot

--- .github/workflows/Invoke-Pester.ps1
+++ .github/workflows/Invoke-Pester.ps1
@@ -1,4 +1,5 @@
 Install-Module Pester,Microsoft.Powershell.SecretManagement -Force -Scope CurrentUser
+Write-Verbose "current location is $($PSScriptRoot)" -Verbose
 Set-Location -Path "$($PSScriptRoot)/../../SecredManagement.Keepass.Extension/Tests"
 $pesterResult = Invoke-Pester -Output Detailed -PassThru
 if ($pesterResult.Result -ne 'Passed') {
