62fd2ef3664b38dc78f3cb5dcca27b3d231acd83
Tom Robijns <trammeke@hotmail.com>
2021-01-14 08:59:17 +0100

    Updated tests for multiple vault parameters
    * Changed initial test cases to "Path Only" tests
    * Added test cases with Path and UseMasterPassword

--- SecretManagement.KeePass.Extension/Tests/Test-SecretVault.Tests.ps1
+++ SecretManagement.KeePass.Extension/Tests/Test-SecretVault.Tests.ps1
@@ -8,36 +8,44 @@ foreach ($mod in (get-module -name *secret*)) { write-verbose $mod.name -verbose
 InModuleScope -ModuleName 'SecretManagement.KeePass.Extension' {
     Describe "Test-SecretVault" {
         BeforeAll {
-            $ModuleName = 'SecretManagement.Keepass.Extension'
-            $MasterKey = '"1}`.2R{LX1`Jm8%XX2/'
-            $KeepassDatabase = "Testdb.kdbx"
-    
-            $VaultName = "KeepassPesterTest_$([guid]::NewGuid())"
-            $VaultPath = Join-Path -Path $TestDrive -ChildPath $KeepassDatabase
-            Copy-Item -Path "$($PSScriptRoot)/Testdb.kdbx" -Destination $VaultPath
-            $VaultKey = [PSCredential]::new('vaultkey',(ConvertTo-SecureString -AsPlainText -Force $MasterKey))
-    
-            $RegisterSecretVaultParams = @{
-                Name            = $VaultName
-                ModuleName      = (Get-Module 'SecretManagement.KeePass').Path
-                PassThru        = $true
-                VaultParameters = @{
-                    Path = $VaultPath
-                }
-            }
-
-            Mock -Verifiable -CommandName 'Get-Credential' -MockWith {$VaultKey}
+            $ModuleName = 'SecretManagement.KeePass'
+            $ModulePath = (Get-Module $ModuleName).Path
+            $BaseKeepassDatabaseName = "Testdb"
         }
         AfterAll {
             try {
                 Microsoft.PowerShell.SecretManagement\Get-SecretVault -Name $VaultName -ErrorAction SilentlyContinue | Microsoft.PowerShell.SecretManagement\Unregister-SecretVault -ErrorAction SilentlyContinue
             } catch [system.Exception] { }
         }
-        Context "Validating Master Key rules" {
+        Context "Validating Path Parameter Only" {
             BeforeAll {
-                $TheVault = Microsoft.PowerShell.SecretManagement\Register-SecretVault @RegisterSecretVaultParams
+                $MasterKey = '"1}`.2R{LX1`Jm8%XX2/'
+                $VaultMasterKey = [PSCredential]::new('vaultkey',(ConvertTo-SecureString -AsPlainText -Force $MasterKey))
+
+                $VaultName = "KeepassPesterTest_$([guid]::NewGuid())"
+                $KeePassDatabaseSuffix = 'PathOnly'
+                $KeePassDatabaseFileName = "$($BaseKeepassDatabaseName)$($KeePassDatabaseSuffix).kdbx"
+                $VaultPath = Join-Path -Path $TestDrive -ChildPath $KeePassDatabaseFileName
+                Copy-Item -Path "$($PSScriptRoot)/$($KeePassDatabaseFileName)" -Destination $VaultPath
+
+                $RegisterSecretVaultPathOnlyParams = @{
+                    Name            = $VaultName
+                    ModuleName      = $ModulePath
+                    PassThru        = $true
+                    VaultParameters = @{
+                        Path = $VaultPath
+                    }
+                }
+                $TheVault = Microsoft.PowerShell.SecretManagement\Register-SecretVault @RegisterSecretVaultPathOnlyParams
+
+                Mock -Verifiable -CommandName 'Get-Credential' -MockWith {$VaultMasterKey}
             }
-            It "Should not have a variable 'Vault_$($VaultName)'" {
+            AfterAll {
+                try {
+                    Microsoft.PowerShell.SecretManagement\Get-SecretVault -Name $VaultName -ErrorAction SilentlyContinue | Microsoft.PowerShell.SecretManagement\Unregister-SecretVault -ErrorAction SilentlyContinue
+                } catch [system.Exception] { }
+            }
+                It "Should not have a variable 'Vault_$($VaultName)'" {
                 { (Get-Variable -Name "Vault_$VaultName" -Scope Script).Value } | Should -Throw
             }
             It "Should request a credential on the first pass" {
@@ -50,6 +58,47 @@ InModuleScope -ModuleName 'SecretManagement.KeePass.Extension' {
             }
             It "Should have a variable 'Vault_$($VaultName)'" {
                 { (Get-Variable -Name "Vault_$VaultName" -Scope Script).Value } | Should -Not -Throw
+            }
+        }
+        Context "Validating Path and UseMasterPassword Parameters" {
+            BeforeAll {
+                $MasterKey = '"1}`.2R{LX1`Jm8%XX2/'
+                $VaultMasterKey = [PSCredential]::new('vaultkey',(ConvertTo-SecureString -AsPlainText -Force $MasterKey))
+
+                $VaultName = "KeepassPesterTest_$([guid]::NewGuid())"
+                $KeePassDatabaseSuffix = 'PathAndUseMasterPassword'
+                $KeePassDatabaseFileName = "$($BaseKeepassDatabaseName)$($KeePassDatabaseSuffix).kdbx"
+                $VaultPath = Join-Path -Path $TestDrive -ChildPath $KeePassDatabaseFileName
+                Copy-Item -Path "$($PSScriptRoot)/$($KeePassDatabaseFileName)" -Destination $VaultPath
+
+                $RegisterSecretVaultPathOnlyParams = @{
+                    Name            = $VaultName
+                    ModuleName      = $ModulePath
+                    PassThru        = $true
+                    VaultParameters = @{
+                        Path = $VaultPath
+                        UseMasterPassword = $true
+                    }
+                }
+                $TheVault = Microsoft.PowerShell.SecretManagement\Register-SecretVault @RegisterSecretVaultPathOnlyParams
+
+                Mock -Verifiable -CommandName 'Get-Credential' -MockWith {$VaultMasterKey}
+            }
+            AfterAll {
+                try {
+                    Microsoft.PowerShell.SecretManagement\Get-SecretVault -Name $VaultName -ErrorAction SilentlyContinue | Microsoft.PowerShell.SecretManagement\Unregister-SecretVault -ErrorAction SilentlyContinue
+                } catch [system.Exception] { }
+            }
+                It "Should not have a variable 'Vault_$($VaultName)'" {
+                { (Get-Variable -Name "Vault_$VaultName" -Scope Script).Value } | Should -Throw
+            }
+            It "Should request a credential on the first pass" {
+                Test-SecretVault -VaultName $VaultName
+                Assert-MockCalled -CommandName 'Get-Credential' -Exactly 1 -Scope Context
+            }
+            It 'Should not request a credential on the second pass' {
+                Test-SecretVault -VaultName $VaultName
+                Assert-MockCalled -CommandName 'Get-Credential' -Exactly 1 -Scope Context
             }
             It "Should have a variable 'Vault_$($VaultName)'" {
                 { (Get-Variable -Name "Vault_$VaultName" -Scope Script).Value } | Should -Not -Throw
