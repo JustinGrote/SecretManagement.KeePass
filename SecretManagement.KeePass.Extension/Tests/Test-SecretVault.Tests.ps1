91c937afddf0cf830d1f42ac0efa02f96db22167
Tom Robijns <trammeke@hotmail.com>
2021-01-14 08:12:44 +0100

    Redesigned my tests
    * Ensured correct modules are preloaded
    * Provided a full path for the Register-SecretVault
    * Correct cleanup of registered vaults
    * Ensure my tests work now

--- SecretManagement.KeePass.Extension/Tests/Test-SecretVault.Tests.ps1
+++ SecretManagement.KeePass.Extension/Tests/Test-SecretVault.Tests.ps1
@@ -1,24 +1,25 @@
 Get-Module *Secret* | Remove-Module -ErrorAction SilentlyContinue -Force
 Import-Module -Name 'Microsoft.PowerShell.SecretManagement'
-Import-Module -Name "$($PSScriptRoot)/../SecretManagement.KeePass.Extension.psd1" -Force
-Import-Module -Name "$($PSScriptRoot)/../../SecretManagement.KeePass.psd1" -force
+Write-Verbose "$($PSScriptRoot)/../SecretManagement.KeePass.Extension.psd1"
+Import-Module -Name "$($PSScriptRoot)/../../SecretManagement.KeePass.psd1" -Force
+
+foreach ($mod in (get-module -name *secret*)) { write-verbose $mod.name -verbose}
 
 InModuleScope -ModuleName 'SecretManagement.KeePass.Extension' {
     Describe "Test-SecretVault" {
         BeforeAll {
-            Import-Module -Name "$($PSScriptRoot)/../../SecretManagement.KeePass.psd1" -Force
             $ModuleName = 'SecretManagement.Keepass.Extension'
             $MasterKey = '"1}`.2R{LX1`Jm8%XX2/'
             $KeepassDatabase = "Testdb.kdbx"
     
             $VaultName = "KeepassPesterTest_$([guid]::NewGuid())"
             $VaultPath = Join-Path -Path $TestDrive -ChildPath $KeepassDatabase
-            Copy-Item -Path "$($psscriptroot)/Testdb.kdbx" -Destination $VaultPath
+            Copy-Item -Path "$($PSScriptRoot)/Testdb.kdbx" -Destination $VaultPath
             $VaultKey = [PSCredential]::new('vaultkey',(ConvertTo-SecureString -AsPlainText -Force $MasterKey))
     
             $RegisterSecretVaultParams = @{
                 Name            = $VaultName
-                ModuleName      = 'SecretManagement.KeePass'
+                ModuleName      = (Get-Module 'SecretManagement.KeePass').Path
                 PassThru        = $true
                 VaultParameters = @{
                     Path = $VaultPath
@@ -26,6 +27,11 @@ InModuleScope -ModuleName 'SecretManagement.KeePass.Extension' {
             }
 
             Mock -Verifiable -CommandName 'Get-Credential' -MockWith {$VaultKey}
+        }
+        AfterAll {
+            try {
+                Microsoft.PowerShell.SecretManagement\Get-SecretVault -Name $VaultName -ErrorAction SilentlyContinue | Microsoft.PowerShell.SecretManagement\Unregister-SecretVault -ErrorAction SilentlyContinue
+            } catch [system.Exception] { }
         }
         Context "Validating Master Key rules" {
             BeforeAll {
@@ -36,11 +42,17 @@ InModuleScope -ModuleName 'SecretManagement.KeePass.Extension' {
             }
             It "Should request a credential on the first pass" {
                 Test-SecretVault -VaultName $VaultName
-                Assert-MockCalled -CommandName 'Get-Credential' -Exactly 1
+                Assert-MockCalled -CommandName 'Get-Credential' -Exactly 1 -Scope Context
             }
             It 'Should not request a credential on the second pass' {
                 Test-SecretVault -VaultName $VaultName
-                Assert-MockCalled -CommandName 'Get-Credential' -Exactly 1
+                Assert-MockCalled -CommandName 'Get-Credential' -Exactly 1 -Scope Context
+            }
+            It "Should have a variable 'Vault_$($VaultName)'" {
+                { (Get-Variable -Name "Vault_$VaultName" -Scope Script).Value } | Should -Not -Throw
+            }
+            It "Should have a variable 'Vault_$($VaultName)'" {
+                { (Get-Variable -Name "Vault_$VaultName" -Scope Script).Value } | Should -Not -Throw
             }
         }
     }
