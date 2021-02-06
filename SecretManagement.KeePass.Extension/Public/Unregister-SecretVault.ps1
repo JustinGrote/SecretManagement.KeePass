a61ef1d55776cedc3222ff1ed65ced846a77634e
Justin Grote <justingrote@users.noreply.github.com>
2021-02-06 15:00:43 -0800

    🩹 Better Unregister-SecretVault

--- SecretManagement.KeePass.Extension/Public/Unregister-SecretVault.ps1
+++ SecretManagement.KeePass.Extension/Public/Unregister-SecretVault.ps1
@@ -4,5 +4,9 @@ function Unregister-SecretVault {
         [string] $VaultName,
         [hashtable] $AdditionalParameters
     )
-    Remove-Variable -Name "Vault_$VaultName" -Scope Script -Force -ErrorAction SilentlyContinue
+    try {
+        Remove-Variable -Name "Vault_$VaultName" -Scope Script -Force -ErrorAction Stop
+    } catch [ItemNotFoundException] {
+        Write-Verbose "Vault ${VaultName}: Vault was not loaded at time of deregistration"
+    }
 }
