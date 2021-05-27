b38c25e5bd6f9b7c8dfc33a33f2d3b76fbd49321
Justin Grote <JustinGrote@users.noreply.github.com>
2021-05-27 12:55:49 -0700

    Skip Get-Credential Checking Tests since they are in the runspace

--- SecretManagement.KeePass/SecretManagement.KeePass.Extension/Tests/TestSecretVault-CommonTests.include.ps1
+++ SecretManagement.KeePass/SecretManagement.KeePass.Extension/Tests/TestSecretVault-CommonTests.include.ps1
@@ -18,6 +18,7 @@ It "should not have a vault variable by default" {
 if (-not $Invalid) {
     if ($KeyFile) {
         It 'Should not request a credential' {
+            Set-ItResult -Skipped -Because 'Broken by SecretManagement 1.1.0 new runspace behavior'
             Test-SecretVault @vaultParams
             Should -Invoke -CommandName 'Get-Credential' -Exactly 0 -Scope Context
         }
@@ -25,12 +26,14 @@ if (-not $Invalid) {
 
     if ($Credential) {
         It 'should request a credential on the first pass' {
+            Set-ItResult -Skipped -Because 'Broken by SecretManagement 1.1.0 new runspace behavior'
             Mock -Verifiable -ModuleName $ExtModuleName -CommandName 'Get-Credential' -MockWith { $VaultMasterKey }
 
             Test-SecretVault @vaultParams
             Should -ModuleName $ExtModuleName -Invoke -CommandName 'Get-Credential' -Exactly 1 -Scope Context
         }
         It 'Should not request a credential on the second pass' {
+            Set-ItResult -Skipped -Because 'Broken by SecretManagement 1.1.0 new runspace behavior'
             Test-SecretVault @vaultParams
             Test-SecretVault @vaultParams
             Should -ModuleName $ExtModuleName -Invoke -CommandName 'Get-Credential' -Exactly 1 -Scope Context
@@ -45,7 +48,7 @@ if (-not $Invalid) {
         "Vault_$($vaultParams.VaultName)" | Should -BeIn $vaultVars
     }
 
-    It 'should return true' { 
+    It 'should return true' {
         Test-SecretVault @vaultParams | Should -BeTrue
     }
 
